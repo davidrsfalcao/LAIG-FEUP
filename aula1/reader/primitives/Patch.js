a0dff4a7d2d1e20811d39b10013d5c707a86a704
David Falcão <up201506571@fe.up.pt>
2017-10-21 13:55:29 +0100

    mudança argumentos patch

--- aula1/reader/primitives/Patch.js
+++ aula1/reader/primitives/Patch.js
@@ -4,10 +4,15 @@
 */
 
 function Patch(scene, degree1, degree2, controlvertexes) {
-  	var knots1 = this.getKnotsVector(degree1); // to be built inside webCGF in later versions ()
-	var knots2 = this.getKnotsVector(degree2); // to be built inside webCGF in later versions
 
-	var nurbsSurface = new CGFnurbsSurface(degree1, degree2, knots1, knots2, controlvertexes); // TODO  (CGF 0.19.3): remove knots1 and knots2 from CGFnurbsSurface method call. Calculate inside method.
+    var tmp = controlvertexes.slice();
+    var a = tmp.length - 1;
+    var b = tmp[0].length - 1;
+
+    var knots1 = this.getKnotsVector(a); // to be built inside webCGF in later versions ()
+    var knots2 = this.getKnotsVector(b); // to be built inside webCGF in later versions
+
+	var nurbsSurface = new CGFnurbsSurface(a, b, knots1, knots2, controlvertexes); // TODO  (CGF 0.19.3): remove knots1 and knots2 from CGFnurbsSurface method call. Calculate inside method.
 	getSurfacePoint = function(u, v) {
 		return nurbsSurface.getPoint(u, v);
 	};
